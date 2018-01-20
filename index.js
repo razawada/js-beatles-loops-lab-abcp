de8909c5600a56a00346bb3d19f73d7912f80dbf
Rance Zawada <razawada@gmail.com>
2018-01-20 01:56:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function iLoveTheBeatles(n) {
-  var response = [];
-  
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = [];
+  for (var i = 0; i < musicians.length; i++) {
+    phrases.push(musicians[i]) + " plays " + instruments[i];
+  }
+  return phrases;
 }
