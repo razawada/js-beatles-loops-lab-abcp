304ac80aee7c5df8c1a217ee295c6072bfc34b71
Rance Zawada <razawada@gmail.com>
2018-01-20 02:00:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ function theBeatlesPlay(musicians, instruments) {
     phrases.push(musicians[i] + " plays " + instruments[i]);
   }
   return phrases;
+};
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian", "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newFacts;
 }
