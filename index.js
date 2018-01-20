e84afc72d6ebea6f5eb8dea7e77e4ad8ec983797
Rance Zawada <razawada@gmail.com>
2018-01-20 02:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-'use strict';
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function theBeatlesPlay(musicians, instruments) {
   var phrases = [];
